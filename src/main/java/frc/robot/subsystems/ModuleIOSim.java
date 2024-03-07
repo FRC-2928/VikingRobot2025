e7307252efec001f1f72124b0fa1747d5c411dab
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2024-03-06 16:56:29 -0800

    removed cancoder from moduleIOSim

--- src/main/java/frc/robot/subsystems/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/ModuleIOSim.java
@@ -15,9 +15,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.*;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import frc.robot.Constants;
 
 public class ModuleIOSim implements ModuleIO {
@@ -25,7 +24,7 @@ public class ModuleIOSim implements ModuleIO {
 
 	private final DCMotorSim drive = new DCMotorSim(DCMotor.getFalcon500(1), 6.75, 0.025);
 	private final DCMotorSim azimuth = new DCMotorSim(DCMotor.getFalcon500(1), 150.0 / 7.0, 0.004);
-	private final EncoderSim cancoder = new EncoderSim(null);
+	//private final EncoderSim cancoder = new EncoderSim(null);
 
 	@Override
 	public void updateInputs(final ModuleIOInputs inputs) {
