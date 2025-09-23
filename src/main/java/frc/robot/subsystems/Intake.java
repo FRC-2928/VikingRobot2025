38349a93202207fcf0eb3598e0dc64c8fed58916
= <=>
2025-09-22 18:16:44 -0700

    Clean up unused commented out stuff from intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -20,10 +20,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-//  Kracken x60 for deploy retract w/ encoder
-//  Kracken x60 for motors (all one)
-// Minion for Belt
-// Minion for trough and limit switch
+
 public class Intake extends SubsystemBase {
 	public static enum Feeder {
 		Reverse(6), Halt(0), Forward(-6); // TODO: negative is currently forward, rework this to not be a hack
@@ -34,24 +31,17 @@ public class Intake extends SubsystemBase {
 			this.voltage = input;
 		}
 	}
+	
 	@AutoLog
 	public static class IntakeInputs {
 		public boolean troughHasCoral = false;
-		// public AngularVelocity intakeSpeed;
-	// public Angle pivotAngle;
 		public AngularVelocity troughSpeed = Units.RotationsPerSecond.zero();
 	}
 
 	public final IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
 
-	// private final TalonFX wheels;
-	// private final TalonFX pivot;
 	private final TalonFXS trough;
 	
-	// private final StatusSignal<AngularVelocity> intakeSpeed;
-	// private final StatusSignal<Angle> pivotAngle;
-	// private TalonFXConfiguration intakeWheelsConfig = new TalonFXConfiguration();
-	// private TalonFXConfiguration pivotWheelConfig = new TalonFXConfiguration();
 	private final StatusSignal<ForwardLimitValue> troughSensor;
 	private final StatusSignal<AngularVelocity> troughSpeed;
 
@@ -84,37 +74,8 @@ public class Intake extends SubsystemBase {
 			.withForwardLimitSource(ForwardLimitSourceValue.LimitSwitchPin)
 			.withReverseLimitEnable(false);
 
-		// // Ground Intake Config
-		// // Ground Intake Wheels Config
-		// intakeWheelsConfig.CurrentLimits.StatorCurrentLimit = 80.0;
-		// intakeWheelsConfig.CurrentLimits.StatorCurrentLimitEnable = true;
-		// intakeWheelsConfig.TorqueCurrent.PeakForwardTorqueCurrent = 40;
-		// intakeWheelsConfig.TorqueCurrent.PeakReverseTorqueCurrent = -40;
-		// // Supply current limits
-		// intakeWheelsConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-		// intakeWheelsConfig.CurrentLimits.SupplyCurrentLimit = 60;  	 // max current draw allowed
-		// intakeWheelsConfig.CurrentLimits.SupplyCurrentLowerLimit = 35;  // current allowed *after* the supply current limit is reached
-		// intakeWheelsConfig.CurrentLimits.SupplyCurrentLowerTime = 0.1;  // max time allowed to draw SupplyCurrentLimit
-		// intakeWheelsConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.1;
-		// intakeWheelsConfig.Audio = Constants.talonFXAudio;
-		// pivotWheelConfig = intakeWheelsConfig;
-		
-		// pivotWheelConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-		// pivotWheelConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = Constants.Intake.max.in(Units.Rotations);
-		// pivotWheelConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
-		// pivotWheelConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Constants.Intake.pivotMin.in(Units.Rotations);
-		// // PID values
-		// pivotWheelConfig.Slot0 =  Constants.Intake.pivotConfig;
-		// intakeWheelsConfig.Slot0 = Constants.Intake.flywheelGainsSlot0;
-
-		// this.wheels.getConfigurator().apply(intakeWheelsConfig);
-		// this.wheels.setNeutralMode(NeutralModeValue.Brake);
-		// this.pivot.getConfigurator().apply(pivotWheelConfig);
-		// this.pivot.setNeutralMode(NeutralModeValue.Brake);
 		this.trough.getConfigurator().apply(troughConfig);
 
-		// this.intakeSpeed = this.wheels.getRotorVelocity();
-		// this.pivotAngle = this.pivot.getRotorPosition();
 		this.troughSpeed = this.trough.getVelocity();
 		this.troughSensor = this.trough.getForwardLimit();
 
@@ -122,11 +83,6 @@ public class Intake extends SubsystemBase {
 			troughSpeed,
 			troughSensor
 		);
-	}
-	
-	public void runIntake(final Feeder demand){
-		// Intake isn't integrated, do nothing
-		// this.wheels.setControl(new VelocityDutyCycle(demand.demand*0.1));
 	}
 
 	public boolean holdingGamePeice() {
